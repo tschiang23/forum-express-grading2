339eb6d8ee036d8b82992a61ec2050e06ceffe53
tschiang23 <tien23@gmail.com>
2024-10-15 11:12:56 +0800

    feat: add user signup

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const admin = require('./modules/admin')
 
 // 載入 controlle
 const restController = require('../controllers/restaurant-controller')
-const admin = require('./modules/admin')
+const userController = require('../controllers/user-controller')
 
+// 管理者路由
 router.use('/admin', admin)
+
+// 帳號註冊登入路由
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 
 router.get('/restaurants', restController.getRestaurants)
 
