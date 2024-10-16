d0864a34c9c21fed953f51957270d6e410283394
tschiang23 <tien23@gmail.com>
2024-10-16 09:34:17 +0800

    feat: add restaurant model

--- config/passport.js
+++ config/passport.js
@@ -16,10 +16,11 @@ passport.use(new LocalStrategy(
     User.findOne({ where: { email } })
       .then(user => {
         if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
-        bcrypt.compare(password, user.password).then(res => {
-          if (!res) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
-          return cb(null, user)
-        })
+        bcrypt.compare(password, user.password)
+          .then(res => {
+            if (!res) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+            return cb(null, user)
+          })
       })
   }
 ))
