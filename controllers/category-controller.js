98658b9911048c8b9c85094ecda7bf82355e3757
tschiang23 <tien23@gmail.com>
2024-10-18 11:12:50 +0800

    feat: update delete category

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -1,4 +1,4 @@
-const { Category } = require('../models')
+const { Category, Restaurant } = require('../models')
 const categoryController = {
   getCategories: (req, res, next) => {
     return Promise.all([
@@ -32,13 +32,31 @@ const categoryController = {
       .catch(err => next(err))
   },
   deleteCategory: (req, res, next) => {
-    return Category.findByPk(req.params.id)
-      .then(category => {
-        if (!category) throw new Error("Category didn't exist!")
-        return category.destroy()
+    return Promise.all([
+      Category.findByPk(req.params.id, { include: [Restaurant] }),
+      Category.findOne({
+        raw: true,
+        where: { name: '未分類' }
       })
+    ]).then(([category, newCategory]) => {
+      if (!category) throw new Error("Category didn't exist!")
+
+      // 如果沒有相關類別的餐廳 直接刪除類別
+      if (category.Restaurants.length === 0) {
+        return category.destroy()
+      } else {
+        // 更新餐廳categoryId
+        return Restaurant.update({ categoryId: newCategory.id }, {
+          where: {
+            categoryId: req.params.id
+          }
+        })
+          .then(() => category.destroy())
+      }
+    })
       .then(() => res.redirect('/admin/categories'))
-      .then(err => next(err))
+      .catch(err => next(err))
   }
 }
+
 module.exports = categoryController
