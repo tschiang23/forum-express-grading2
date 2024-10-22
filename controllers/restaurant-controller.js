d7fb77ec813e0bcb7f74e73b8372992973072275
tschiang23 <tien23@gmail.com>
2024-10-22 10:33:20 +0800

    feat:get user's favorited restaurants & switch button

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -22,9 +22,14 @@ const restaurantController = {
       Category.findAll({ raw: true })
     ])
       .then(([restaurants, categories]) => {
+        // 整理user favoritedRestaurantsId資料
+        const favoritedRestaurantsId = req.user && req.user.FavoritedRestaurants.map(fr => fr.id)
+
+        // 整理restaurants資料
         const data = restaurants.rows.map(r => ({
           ...r,
-          description: r.description.substring(0, 50)
+          description: r.description.substring(0, 50),
+          isFavorited: favoritedRestaurantsId.includes(r.id)
         }))
         return res.render('restaurants', {
           restaurants: data,
@@ -39,7 +44,8 @@ const restaurantController = {
     return Restaurant.findByPk(req.params.id, {
       include: [
         Category,
-        { model: Comment, include: User }
+        { model: Comment, include: User },
+        { model: User, as: 'FavoritedUsers' }
       ],
       order: [[Comment, 'created_at', 'DESC']]
     })
@@ -48,11 +54,15 @@ const restaurantController = {
 
         return restaurant.increment('viewCounts')
       })
-      .then(restaurant =>
+      .then(restaurant => {
+        // 檢查餐廳FavoritedUsers 是否有當前登入的user
+        const isFavorited = restaurant.FavoritedUsers.some(f => f.id === req.user.id)
+
         res.render('restaurant', {
-          restaurant: restaurant.toJSON()
+          restaurant: restaurant.toJSON(),
+          isFavorited
         })
-      )
+      })
       .catch(err => next(err))
   },
   getDashboard: (req, res, next) => {
